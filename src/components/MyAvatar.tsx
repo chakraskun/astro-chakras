d1d9d412cb7d67da9ddc13a89353a5852cf68204
Chakras Kuntjoro <chakras@mid-stay.com>
2024-01-21 14:30:27 +0700

    change avatar image

--- src/components/MyAvatar.tsx
+++ src/components/MyAvatar.tsx
@@ -7,7 +7,7 @@ import {
 export function MyAvatar() {
   return (
     <Avatar className='h-24 w-24'>
-      <AvatarImage src="../public/chakras.jpeg" alt="chakras" />
+      <AvatarImage src='https://avatars.githubusercontent.com/u/62203303?s=400&u=83f88bcb765c9db53b29b9385673fce13a52bc03&v=4' alt="chakras" />
       <AvatarFallback>CAK</AvatarFallback>
     </Avatar>
   )
